b70c2dda509488ce9742c090bea858c0109bfc12
Carter Watts <cdwlions2016@gmail.com>
2019-05-10 17:11:30 -0400

    Initial with empty pod class and motor test class

---
+++ pod/pod.ino
@@ -0,0 +1,22 @@
+#include <DualVNH5019MotorShield.h>
+
+//Decide radio lib between lite - base - mesh and whatever the other one was
+
+/*
+ * Mini Pod Code
+ * V 0.1
+ * 
+ * Carter Watts
+ * 
+ * Notes:
+ */
+
+void setup() {
+ 
+
+}
+
+void loop() {
+
+  
+}
