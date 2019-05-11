62c4653acb57ac3664cac3c376c5d657d0f09e9b
CarterTheGreat <cdwlions2016@gmail.com>
2019-05-11 17:22:11 -0400

    First full pod test with glove written

--- pod/pod.ino
+++ pod/pod.ino
@@ -24,16 +24,25 @@
   String runningS, xS ,yS, zS, f1S, f2S;
 
 //Motor
-
+  DualVNH5019MotorShield md;
+  //Which side of the robot the pod is - needed for arcade control
+  const string LEFT = "left";
+  const string RIGHT = "right";
+  
+  //String side = LEFT;
+  //String side = RIGHT;
+  
 void setup() {
   
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  radio.begin();
-  radio.openWritingPipe(addresses[0]); // 00001
-  radio.openReadingPipe(1, addresses[1]); // 00002
-  radio.setPALevel(RF24_PA_MIN);
-  
+  //Radio
+    radio.begin();
+    radio.openWritingPipe(addresses[0]); // 00001
+    radio.openReadingPipe(1, addresses[1]); // 00002
+    radio.setPALevel(RF24_PA_MIN);
+  //Motor Driver  
+    md.init();
 }
 
 void loop(){
@@ -47,38 +56,55 @@ void loop(){
       radio.read(&data, sizeof(data));
   
       //Indexing
-      ind1 = data.indexOf('/');
-      ind2 = data.indexOf('/',ind1+1);
-      ind3 = data.indexOf('/',ind2+1);
-      ind4 = data.indexOf('/',ind3+1);
-      ind5 = data.indexOf('/',ind4+1);
-      endInd = data.indexOf('>');
+        ind1 = data.indexOf('/');
+        ind2 = data.indexOf('/',ind1+1);
+        ind3 = data.indexOf('/',ind2+1);
+        ind4 = data.indexOf('/',ind3+1);
+        ind5 = data.indexOf('/',ind4+1);
+        endInd = data.indexOf('>');
   
       //String Data
-      runningS = data.substring(startInd+1,ind1);
-      xS = data.substring(ind1+1,ind2);
-      yS = data.substring(ind2+1,ind3);
-      zS = data.substring(ind3+1,ind4);
-      f1S = data.substring(ind4+1,endInd);
-      f2S = data.substring(ind5+1,endInd);
+        runningS = data.substring(startInd+1,ind1);
+        xS = data.substring(ind1+1,ind2);
+        yS = data.substring(ind2+1,ind3);
+        zS = data.substring(ind3+1,ind4);
+        f1S = data.substring(ind4+1,endInd);
+        f2S = data.substring(ind5+1,endInd);
       
       //Int data
-      runningB = runningS.toInt();
-      x = xS.toInt();
-      y = yS.toInt();
-      z = zS.toInt();
-      f1 = f1S.toInt();
-      f2 = f2S.toInt();
+        runningB = runningS.toInt();
+        x = xS.toInt();
+        y = yS.toInt();
+        z = zS.toInt();
+        f1 = f1S.toInt();
+        f2 = f2S.toInt();
 
-      //Control motors here :)
+      //Control motors
+
+        if(x < 30 && x > -30)
+          x = 0;
+        if(y < 30 && x > -30)
+          y = 0;
       
+        xIn = -x;
+        yIn = -y;    
+        //For left
+        if(type == LEFT)
+          //md.setSpeed(-y+x);
+          md.setSpeeds(yIn-xIn);
+  
+        //For right
+        if(side == RIGHT)
+          //md.setSpeed(-y-x);
+          md.setSpeeds(yIn+xIn);
+
     }
 
   //Response
-  delay(5);
-  radio.stopListening();
-  if(dataOutB)
-    radio.write(&dataOut, sizeof(dataOut));
+    delay(5);
+    radio.stopListening();
+    if(dataOutB)
+      radio.write(&dataOut, sizeof(dataOut));
   
 }
 
@@ -86,4 +112,17 @@ void respond(String response){
   dataOutB = true;
   dataOut = response;
 
+}
+
+void stopIfFault(){
+  
+  if (md.getM1Fault()){
+    Serial.println("M1 fault");
+    while(1);
+  }
+  if (md.getM2Fault()){
+    Serial.println("M2 fault");
+    while(1);
+  }
+  
 }
